fabd9d83dd264a75046e2e8a972437101d0767f6
takumi20210430 <uniquetime.t@gmail.com>
2021-05-08 11:34:25 +0000

    [Start]kadai3,6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,10 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back fallback_location: books_path
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
   
   def destroy
