516603c8f6d6c1c538830aa8a30589b22505a8bd
takumi20210430 <uniquetime.t@gmail.com>
2021-05-10 07:39:34 +0000

    [Start]kadai-3,5

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,26 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  #class Follower
   #フォローする側のuser側からみたRelationshipをactive_relationshipsと命名しておく（なんでも可）親（foreign_key）はフォローする側
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
             #↑の中間table（active_relationships）を介してfollowerモデルのuserを集めることをfollowedsと定義する。
   has_many :followeds, through: :active_relationships, source: :followed
 
+  #class Followed
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
+  
   attachment :profile_image, destroy: false
 
   def followed_by?(user)
     #follow済みかどうかしらべる
     passive_relationships.find_by(follower_id: user.id).present?
   end
+
+  
+  #User.find(1).followed_by(2)
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
    validates :introduction, length:{maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'searches/search'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
